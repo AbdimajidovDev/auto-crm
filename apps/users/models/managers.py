de871243c739b325a890984d533d955d774403e7
To'lqinjon <tulqinjonabdimajidov@gmail.com>
2026-04-06 09:57:01 +0500

    fixed

--- apps/users/models/managers.py
+++ apps/users/models/managers.py
@@ -25,7 +25,6 @@ class UserManager(BaseUserManager):
     def create_superuser(self, phone_number, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("role", "su")
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError("Superuser must have is_staff=True")
