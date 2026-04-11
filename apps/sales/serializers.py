e40ea8c8a303a126919e355ecb427ab25dbb7eed
To'lqinjon <tulqinjonabdimajidov@gmail.com>
2026-04-11 11:20:46 +0500

    fixed

--- apps/sales/serializers.py
+++ apps/sales/serializers.py
@@ -36,9 +36,8 @@ class SaleListSerializer(serializers.ModelSerializer):
         return obj.seller.full_name if obj.seller else None
 
     def get_debt(self, obj):
-        return "0.0"
-        # debt = obj.total_amount - obj.paid_amount
-        # return debt if debt > 0 else None
+        debt = obj.total_amount - obj.paid_amount
+        return debt if debt > 0 else None
 
 
 class SaleItemInputSerializer(serializers.Serializer):
