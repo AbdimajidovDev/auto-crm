631e7cd87e8304b44e1896f71df5c2c4d3428614
To'lqinjon <tulqinjonabdimajidov@gmail.com>
2026-04-13 15:24:27 +0500

    real time notification added in transfer

--- apps/transfer/models.py
+++ apps/transfer/models.py
@@ -53,3 +53,39 @@ class StockTransferItem(models.Model):
 
     def __str__(self):
         return f"{self.product.name} {self.quantity}."
+
+
+
+class Notification(TimestampMixin):
+
+    class Type(models.TextChoices):
+        TRANSFER_CREATED = "tc", "Transfer Created"
+        TRANSFER_APPROVED = "ta", "Transfer Approved"
+        TRANSFER_REJECTED = "tr", "Transfer Rejected"
+
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="notifications"
+    )
+
+    type = models.CharField(max_length=20, choices=Type.choices)
+
+    title = models.CharField(max_length=255)
+    message = models.TextField()
+
+    is_read = models.BooleanField(default=False)
+
+    # 🔗 optional reference
+    transfer = models.ForeignKey(
+        "transfer.StockTransfer",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        db_table = "notification"
+        indexes = [
+            models.Index(fields=["user", "is_read"]),
+        ]
