3b567cc96e1d236b93d39eb994fde19d4bd9b4ff
To'lqinjon <tulqinjonabdimajidov@gmail.com>
2026-04-17 16:21:52 +0500

    reports part added

--- apps/reports/urls.py
+++ apps/reports/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from apps.reports.views import DashboardReportAPIView, DashboardAPIView
+from apps.reports.views.report_view import ReportsAPIView
 from apps.reports.views.top_product_view import TopProductsAPIView
 
 urlpatterns = [
+    # Dashboard reports
     # path('', DashboardReportAPIView.as_view()),
-    path('', DashboardAPIView.as_view()),
+    path('dashboard/', DashboardAPIView.as_view()),
     path('top-products/', TopProductsAPIView.as_view()),
+
+    # Reports
+    path('', ReportsAPIView.as_view()),
 ]
