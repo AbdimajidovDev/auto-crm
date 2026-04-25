b60c20b8bc1eafe80b66f282f9b01a8aafc1f83c
To'lqinjon <tulqinjonabdimajidov@gmail.com>
2026-04-25 10:36:13 +0500

    inventory fixed

--- apps/inventory/services/inventory_hooks_service.py
+++ apps/inventory/services/inventory_hooks_service.py
@@ -1,6 +1,7 @@
 from apps.inventory.models import InventorySession, InventoryMovement
 
 
+# Invetarizatsiya jarayonida mahsulot sotilganda miqdordan ayirish
 def handle_sale_item(sale_item):
 
     session = InventorySession.objects.filter(
@@ -20,6 +21,7 @@ def handle_sale_item(sale_item):
     )
 
 
+# Invetarizatsiya jarayonida boshqa do'konga transfer qilinganda miqdordan ayirish
 def handle_transfer_approved(transfer):
 
     session = InventorySession.objects.filter(
@@ -40,6 +42,28 @@ def handle_transfer_approved(transfer):
         )
 
 
+# Invetarizatsiya jarayonida Do'konga transfer qilinganda miqdorgaga qo'shish
+def handle_transfer_in(transfer):
+
+    session = InventorySession.objects.filter(
+        store=transfer.to_store,
+        status="active"
+    ).first()
+
+    if not session:
+        return
+
+    for item in transfer.items.all():
+        InventoryMovement.objects.create(
+            session=session,
+            product=item.product,
+            quantity=item.quantity,
+            type=InventoryMovement.Type.TRANSFER_IN,
+            ref_id=transfer.id
+        )
+
+
+# Invetarizatsiya jarayonida mahsulot vazvrat qilinganda miqdorga qo'shish
 def handle_sale_return(return_obj, sale_item, quantity):
 
     session = InventorySession.objects.filter(
@@ -57,3 +81,25 @@ def handle_sale_return(return_obj, sale_item, quantity):
         type=InventoryMovement.Type.RETURN,
         ref_id=return_obj.id
     )
+
+
+# Invetarizatsiya jarayonida Kirim qilinganda miqdorgaga qo'shish
+def handle_stock_entry(entry):
+
+    session = InventorySession.objects.filter(
+        store=entry.store,
+        status="active"
+    ).first()
+
+    if not session:
+        return
+
+    for item in entry.items.all():
+        InventoryMovement.objects.create(
+            session=session,
+            product=item.product,
+            quantity=item.quantity,
+            type=InventoryMovement.Type.ENTRY,
+            ref_id=entry.id
+        )
+
