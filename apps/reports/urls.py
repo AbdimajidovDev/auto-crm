5f75b401a4ebc9272e8b8f406e8f6da3876ec3f9
To'lqinjon <tulqinjonabdimajidov@gmail.com>
2026-05-22 12:18:27 +0500

    fixed

--- apps/reports/urls.py
+++ apps/reports/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from apps.reports.views import DashboardReportAPIView, DashboardAPIView
+from apps.reports.views import DashboardAPIView
 from apps.reports.views.export_view import ReportsExcelExportAPIView
 from apps.reports.views.report_view import ReportsAPIView
 from apps.reports.views.top_product_view import TopProductsAPIView
 
 urlpatterns = [
     # Dashboard reports
-    # path('', DashboardReportAPIView.as_view()),
     path('dashboard/', DashboardAPIView.as_view()),
     path('top-products/', TopProductsAPIView.as_view()),
 
