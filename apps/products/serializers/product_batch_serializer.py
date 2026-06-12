118c70a232923b40c3a5b4aeee5ee169ddb6767a
To'lqinjon <tulqinjonabdimajidov@gmail.com>
2026-06-12 16:52:22 +0500

    fixed

--- apps/products/serializers/product_batch_serializer.py
+++ apps/products/serializers/product_batch_serializer.py
@@ -40,7 +40,7 @@ class ProductLocationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ProductLocation
-        fields = ("id", "location", "description")
+        fields = ("id", "location_uz", "location_cyrl", "description_uz", "description_cyrl")
 
 
 # class ProductLocationSerializer(serializers.ModelSerializer):
