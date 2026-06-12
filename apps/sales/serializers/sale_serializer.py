d3aa33db7e3ccfcef3ee4234b96301224b13d17b
To'lqinjon <tulqinjonabdimajidov@gmail.com>
2026-06-12 10:58:01 +0500

    fixed

--- apps/sales/serializers/sale_serializer.py
+++ apps/sales/serializers/sale_serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
-
+from django.utils import timezone
 from apps.sales.models import (
     Sale,
     SaleItem,
